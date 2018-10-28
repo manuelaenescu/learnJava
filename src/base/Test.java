fd9f9bf9058f17f934bfdf1339fe8861bc82ad56
manuelaenescu <you@example.com>
2018-10-28 16:02:23 +0200

    loops basic example

---
+++ src/base/Test.java
@@ -0,0 +1,13 @@
+package base;
+
+public class Test {
+
+    public static void  main (String... args){
+
+        Loops loops = new Loops ();
+
+        loops.printSimpleArray();
+
+        System.out.println(Loops.message());
+    }
+}
