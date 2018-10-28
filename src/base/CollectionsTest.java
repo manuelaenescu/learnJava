6719928ddb314a8cab9c1b9c33c77ca90f30fa41
manuelaenescu <you@example.com>
2018-10-28 17:41:46 +0200

    added collection examples: list, map, set

---
+++ src/base/CollectionsTest.java
@@ -0,0 +1,52 @@
+package base;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+public class CollectionsTest {
+
+
+
+    public static void main (String[] args){
+
+        listTest();
+
+        TreeMap<Integer, String> mapOfString = new TreeMap<>();
+
+
+
+        mapOfString.put(2, "second");
+        mapOfString.put(3, "third");
+        mapOfString.put(1, "first");
+
+        mapOfString.put(1, "newfirst");
+
+        Set<Integer> keys = mapOfString.keySet();
+        System.out.println("Keys" + keys);
+        System.out.println("Values" + mapOfString.values());
+
+        for (int key : keys){
+
+            System.out.println(key + ":" + mapOfString.get(key));
+        }
+
+
+
+
+
+
+
+    }
+
+    private static void listTest() {
+        ArrayList<String> listOfStrings = new ArrayList<>();
+
+        listOfStrings.add("first");
+        listOfStrings.add("second");
+        listOfStrings.add("third");
+
+        for (int i=0; i < listOfStrings.size(); i++)
+            System.out.println(listOfStrings.get(i));
+    }
+}
